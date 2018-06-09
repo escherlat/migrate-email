90b2853fc6bf73c4c7dccbc65733a8b55390d709
Ken Power <kenp@cpanel.net>
2018-06-09 11:00:12 -0500

    Tidy

--- src/MigrateMail.pm
+++ src/MigrateMail.pm
@@ -20,43 +20,42 @@ sub domigrateuser {
 
     my ( $remoteuser, $remotepass, $localuser, $localpass, $mailservice ) = $args->get( 'remoteEmail', 'remotePassword', 'localEmail', 'localPassword', 'mailService' );
 
-    if( ! $remoteuser || $remoteuser !~ m/\@/ ){
-        $result->error( 'Remote Username must be an email account');
+    if ( !$remoteuser || $remoteuser !~ m/\@/ ) {
+        $result->error('Remote Username must be an email account');
         return;
     }
-    if( !$remotepass ){
-        $result->error( 'Remote Password was not provided' );
-        return;
-    }
-
-    if( ! $localuser || $localuser !~ m/\@/ ){
-        $result->error( 'Local user must be an email account' );
+    if ( !$remotepass ) {
+        $result->error('Remote Password was not provided');
         return;
     }
 
-    if( !$localpass ){
-        $result->error( 'Local Password was not provided' );
+    if ( !$localuser || $localuser !~ m/\@/ ) {
+        $result->error('Local user must be an email account');
         return;
     }
 
-    if( ! $mailservice ){
-        $result->error( 'Please select a mail service');
+    if ( !$localpass ) {
+        $result->error('Local Password was not provided');
+        return;
+    }
+
+    if ( !$mailservice ) {
+        $result->error('Please select a mail service');
         return;
     }
 
     my $remoteserver = 'imap.gmail.com';
     my $localserver  = 'localhost';
-    my $remoteport = 993;
-    my $_services = Cpanel::JSON::LoadFile( $_services_file );
-    if( exists $_services->{ $mailservice } ){
-        $remoteserver = $_services->{ $mailservice }->{'server'};
-        $remoteport = $_services->{ $mailservice }->{'port'};
+    my $remoteport   = 993;
+    my $_services    = Cpanel::JSON::LoadFile($_services_file);
+    if ( exists $_services->{$mailservice} ) {
+        $remoteserver = $_services->{$mailservice}->{'server'};
+        $remoteport   = $_services->{$mailservice}->{'port'};
     }
-
 
     my $imapsync = Cpanel::FindBin::findbin('imapsync');
 
-    my $localport  = '993';
+    my $localport = '993';
 
     my @defaultoptions = ( '--automap', '--syncinternaldates', '--ssl1', '--ssl2', '--noauthmd5', '--exclude', 'All Mail|Spam|Trash', '--allowsizemismatch', '--logdir', '/tmp', '--logfile', 'imapsync.txt' );
 
@@ -65,9 +64,9 @@ sub domigrateuser {
     my @largemailboxopts = ('--split1 100 --split2 100');
 
     my $result = Cpanel::SafeRun::Simple::saferun( $imapsync, @mailoptions, @defaultoptions );
-    
-    if( $result !~ m/detected 0 errors/im ){
-        $result->error( 'Failed to transfer email');
+
+    if ( $result !~ m/detected 0 errors/im ) {
+        $result->error('Failed to transfer email');
         return;
     }
     return 1;
@@ -76,9 +75,9 @@ sub domigrateuser {
 sub services {
     my ( $args, $result ) = @_;
 
-    my $_services = Cpanel::JSON::LoadFile( $_services_file );
+    my $_services = Cpanel::JSON::LoadFile($_services_file);
 
-    my @services = sort keys %{ $_services };
+    my @services = sort keys %{$_services};
 
     $result->data(
         \@services,
